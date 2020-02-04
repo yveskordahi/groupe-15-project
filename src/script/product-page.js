4f9e670838f509e2cb31d2ff19f44b75dfbfec9e
unknown <y.kordahi@outlook.fr>
2020-02-04 16:06:40 +0100

    fix reference img

--- src/script/product-page.js
+++ src/script/product-page.js
@@ -1,3 +1,15 @@
+let blackMaskBlue = require("../img/black-mask-blue-logo.png");
+let blackMaskRed = require("../img/black-mask-red-logo.png");
+let blackMaskYellow = require("../img/black-mask-yellow-logo.png");
+let blackMaskGreen = require("../img/black-mask-green-logo.png");
+let blackMaskPink = require("../img/black-mask-pink-logo.png");
+
+let WhiteMaskRed = require("../img/white-mask-red-logo.png");
+let WhiteMaskBlue = require("../img/white-mask-blue-logo.png");
+let WhiteMaskGreen = require("../img/white-mask-green-logo.png");
+let WhiteMaskYellow = require("../img/white-mask-yellow-logo.png");
+let WhiteMaskPink = require("../img/white-mask-pink-logo.png");
+
 $(document).ready(function() {
   counter = 2;
 
@@ -29,44 +41,44 @@ $(document).ready(function() {
   });
 
   $(".mask-black-product1 .color-1").click(function() {
-    $(".row--maskImg-black").attr("src", "img/black-mask-blue-logo.png");
+    $(".row--maskImg-black").attr("src", blackMaskBlue);
   });
 
   $(".mask-black-product1 .color-2").click(function() {
-    $(".row--maskImg-black").attr("src", "img/black-mask-red-logo.png");
+    $(".row--maskImg-black").attr("src", blackMaskRed);
   });
 
   $(".mask-black-product1 .color-3").click(function() {
-    $(".row--maskImg-black").attr("src", "img/black-mask-yellow-logo.png");
+    $(".row--maskImg-black").attr("src", blackMaskYellow);
   });
 
   $(".mask-black-product1 .color-4").click(function() {
-    $(".row--maskImg-black").attr("src", "img/black-mask-green-logo.png");
+    $(".row--maskImg-black").attr("src",blackMaskGreen);
   });
 
   $(".mask-black-product1 .color-5").click(function() {
-    $(".row--maskImg-black").attr("src", "img/black-mask-pink-logo.png");
+    $(".row--maskImg-black").attr("src", blackMaskPink);
   });
 
   // -------------------White--------------------
 
   $(".mask-black-product1 .color-1").click(function() {
-    $(".row--maskImg-white").attr("src", "img/white-mask-blue-logo.png");
+    $(".row--maskImg-white").attr("src", WhiteMaskBlue);
   });
 
   $(".mask-black-product1 .color-2").click(function() {
-    $(".row--maskImg-white").attr("src", "img/white-mask-red-logo.png");
+    $(".row--maskImg-white").attr("src", WhiteMaskRed);
   });
 
   $(".mask-black-product1 .color-3").click(function() {
-    $(".row--maskImg-white").attr("src", "img/white-mask-yellow-logo.png");
+    $(".row--maskImg-white").attr("src", WhiteMaskYellow);
   });
 
   $(".mask-black-product1 .color-4").click(function() {
-    $(".row--maskImg-white").attr("src", "img/white-mask-green-logo.png");
+    $(".row--maskImg-white").attr("src", WhiteMaskGreen);
   });
 
   $(".mask-black-product1 .color-5").click(function() {
-    $(".row--maskImg-white").attr("src", "img/white-mask-pink-logo.png");
+    $(".row--maskImg-white").attr("src", WhiteMaskPink);
   });
 });
