529d14928a6485d0fead31629b3c688eba6c44a5
Andrija <andrija.pejic@hetic.net>
2020-02-04 14:41:21 +0100

    fixing problem img

--- src/script/product-page.js
+++ src/script/product-page.js
@@ -29,44 +29,44 @@ $(document).ready(function() {
   });
 
   $(".mask-black-product1 .color-1").click(function() {
-    $(".row--maskImg-black").attr("src", "../img/black-mask-blue-logo.png");
+    $(".row--maskImg-black").attr("src", "img/black-mask-blue-logo.png");
   });
 
   $(".mask-black-product1 .color-2").click(function() {
-    $(".row--maskImg-black").attr("src", "../img/black-mask-red-logo.png");
+    $(".row--maskImg-black").attr("src", "img/black-mask-red-logo.png");
   });
 
   $(".mask-black-product1 .color-3").click(function() {
-    $(".row--maskImg-black").attr("src", "../img/black-mask-yellow-logo.png");
+    $(".row--maskImg-black").attr("src", "img/black-mask-yellow-logo.png");
   });
 
   $(".mask-black-product1 .color-4").click(function() {
-    $(".row--maskImg-black").attr("src", "../img/black-mask-green-logo.png");
+    $(".row--maskImg-black").attr("src", "img/black-mask-green-logo.png");
   });
 
   $(".mask-black-product1 .color-5").click(function() {
-    $(".row--maskImg-black").attr("src", "../img/black-mask-pink-logo.png");
+    $(".row--maskImg-black").attr("src", "img/black-mask-pink-logo.png");
   });
 
   // -------------------White--------------------
 
   $(".mask-black-product1 .color-1").click(function() {
-    $(".row--maskImg-white").attr("src", "../img/white-mask-blue-logo.png");
+    $(".row--maskImg-white").attr("src", "img/white-mask-blue-logo.png");
   });
 
   $(".mask-black-product1 .color-2").click(function() {
-    $(".row--maskImg-white").attr("src", "../img/white-mask-red-logo.png");
+    $(".row--maskImg-white").attr("src", "img/white-mask-red-logo.png");
   });
 
   $(".mask-black-product1 .color-3").click(function() {
-    $(".row--maskImg-white").attr("src", "../img/white-mask-yellow-logo.png");
+    $(".row--maskImg-white").attr("src", "img/white-mask-yellow-logo.png");
   });
 
   $(".mask-black-product1 .color-4").click(function() {
-    $(".row--maskImg-white").attr("src", "../img/white-mask-green-logo.png");
+    $(".row--maskImg-white").attr("src", "img/white-mask-green-logo.png");
   });
 
   $(".mask-black-product1 .color-5").click(function() {
-    $(".row--maskImg-white").attr("src", "../img/white-mask-pink-logo.png");
+    $(".row--maskImg-white").attr("src", "img/white-mask-pink-logo.png");
   });
 });
